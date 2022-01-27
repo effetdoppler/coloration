9fd57841f37d67aa7026f9e28f48769cf8d5b6e4
Michel Zhang <michel529020@gmail.com>
2022-01-27 04:00:05 +0100

    add test.py

--- s4-2025-coloration-main/test.py
+++ s4-2025-coloration-main/test.py
@@ -1,3 +1,17 @@
-from s4-2025-coloration-main import *
+from coloration_program import *
+from testcolor import *
+
 if __name__ == '__main__':
-    run_verif_coloration(color_dsatur, "s4-2025-coloration-main/files")
+    files = "files"
+
+    print("greedy")
+    print(run_verif_coloration(color_greedy, files))
+
+    print("dsatur")
+    print(run_verif_coloration(color_dsatur, files))
+
+    print("weslsh_powell")
+    print(run_verif_coloration(color_weslsh_powell, files))
+
+    print("rlf")
+    print(run_verif_coloration(color_rlf, files))
